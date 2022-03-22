3deae75a3c312fcfbad6978b7c787e27ca2e1d85
mrleafaro <91629924+mrleafaro@users.noreply.github.com>
2022-03-22 19:03:28 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIAVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("A V I R A E L V", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+// adicione código para exibir o restante dos nomes e as dicas.
+    fill("white")
+    textSize(15)
+    text("U N Ç Ã O F",700,150)
+    fill("lightblue")
+    text("Dica: Realiza uma tarefa em particular", 700,170)
+    fill("white")
+    textSize(15)
+    text("A B N C E D O A D O S D",100,250)
+    fill("lightblue")
+    text("Dica: Guarda toda a informação", 100,270)
+    
+    
+
+}
